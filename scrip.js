c254c33afa6d1bf2d33eaf07e7cba52beb5eb416
NicolasPalia <94577574+NicolasPalia@users.noreply.github.com>
2021-11-22 03:45:26 -0300

    Desafio 1 !

---
+++ scrip.js
@@ -0,0 +1,12 @@
+let nombre = (prompt("Ingresa Un nombre"))
+
+let apellido = (prompt("Ingresa Un Apellido"))
+
+console.log (`${nombre} ${apellido}`)
+
+let numero1 = parseFloat (prompt("Gastos de Transporte"))
+let numero2 = parseFloat (prompt("Gastos de Luz"))
+let numero3 = parseFloat (prompt("Gastos de Internet y Telefono"))
+let numero4 = parseFloat (prompt("Gastos de Comida"))
+
+alert (numero1 + numero2 + numero3 + numero4)
