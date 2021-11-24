3c8f38cedc4b025df872c73cda70b3e0fc0c9bc1
NicolasPalia <94577574+NicolasPalia@users.noreply.github.com>
2021-11-24 18:35:44 -0300

    revison logica condicional

--- Desafio 2/scrip.js
+++ Desafio 2/scrip.js
@@ -2,24 +2,29 @@ let empleo = prompt("Tiene Empleo?")
 
 if(empleo === "si" ) {
     console.log(alert("Aplica a los requisitos del prestamo."))
-} else {
+
+    let monto = parseInt(prompt("Ingrese el monto del prestamo que desea."))
+
+    if(monto >= 1000 && monto < 10000 ){
+        console.log(alert("El monto que usted ingresa es demasiado pequeño."))
+    } 
+    else if(monto > 10000 && monto <= 20000){
+        console.log(alert("Usted posee 3 cuotas con interes"))
+    }
+     else if(monto > 20000 &&  monto <= 50000) {
+        console.log(alert("Usted posee 12 Cuotas sin interes"))
+    }
+     else if(monto > 50000 ) {
+        console.log(alert("Usted acaba de exceder el limite del monto que se puede solicitar"))
+    }
+     else {
+        console.log(alert ("Ingrese una cifra valida"))
+    } 
+}
+ else if(empleo === "no"){
     console.log(alert("No aplica a los requisitos del prestamo"))
 }
-
-let monto = parseInt(prompt("Ingrese el monto del prestamo que desea."))
-
-if(monto >= 1000 && monto < 10000 ){
-    console.log(alert("El monto que usted ingresa es demasiado pequeño."))
-} 
-else if(monto > 10000 && monto <= 20000){
-    console.log(alert("Usted posee 3 cuotas con interes"))
-}
- else if(monto > 20000 &&  monto <= 50000) {
-    console.log(alert("Usted posee 12 Cuotas sin interes"))
-}
- else if(monto > 50000 ) {
-    console.log(alert("Usted acaba de exceder el limite del monto que se puede solicitar"))
-}
  else {
-    console.log(alert ("Ingrese una cifra valida"))
-}
+    console.log(alert("Ingrese una respuesta de si o no"))
+ }
+
