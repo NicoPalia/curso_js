d7ccf77cd098cc9faf45e0304f9c24edc84b284a
NicolasPalia <94577574+NicolasPalia@users.noreply.github.com>
2021-12-29 22:00:10 -0300

    Re modificacion desafio 10

--- Desafio 10/scrip.js
+++ Desafio 10/scrip.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
     $('#boton-guardar').click(function () {
-        var nom = $("#nombretxt").val();
-        var apel = $("#apellidotxt").val();
-
-        localStorage.setItem("Nombre", nom);
-        localStorage.setItem("Apellido", apel);
-
-        $("#nombretxt").val("");
-        $("#apellidotxt").val("");
+        var mar = $("#Marcatxt").val();
+        var col = $("#Colortxt").val();
+        var tip = $("#Tipotxt").val();
+        
+        localStorage.setItem("Marca", mar);
+        localStorage.setItem("Color", col);
+        localStorage.setItem("Tipo", tip);
+        
+        $("#Marcatxt").val("");
+        $("#Colortxt").val("");
+        $("#Tipotxt").val("");
     });
 });
 
 $(document).ready(function () {
     $('#boton-cargar').click(function () {
+        
         /*Obtener datos almacenados*/
-        var nombre = localStorage.getItem("Nombre");
-        var apellido = localStorage.getItem("Apellido");
+        var marca = localStorage.getItem("Marca");
+        var color = localStorage.getItem("Color");
+        var tipo = localStorage.getItem("Tipo");
+        
         /*Mostrar datos almacenados*/
-        $("#nombre").append(nombre)
-        $("#apellido").append(apellido)
+        $("#Marca").append(marca)
+        $("#Color").append(color)
+        $("#Tipo").append(tipo) 
+
     });
 });
